b4ade0489981e16419df7fee0b7229331989e9d1
zhefano <cubza@protonmail.com>
2025-02-11 03:21:03 +0100

    la till routes för main, details, about me, och new post components

--- blog/src/app/app.routes.ts
+++ blog/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { DetailsComponent } from './details/details.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import { AuthGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: 'aboutme', component: AboutMeComponent },
+  { path: 'addPost', component: NewPostComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
+];
